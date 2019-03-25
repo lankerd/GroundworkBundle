97d7688691f6e442696940a73a2b0d792de2f833
Julian Lankerd <julianlankerd@gmail.com>
2019-03-25 15:32:51 -0400

    Chawed over to using bindValue in Table Delete Command

--- Command/GroundworkTableDeleteCommand.php
+++ Command/GroundworkTableDeleteCommand.php
@@ -5,11 +5,11 @@ namespace Lankerd\GroundworkBundle\Command;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class GroundworkTableDeleteCommand extends ContainerAwareCommand
 {
+
     protected function configure()
     {
         $this
@@ -20,15 +20,17 @@ class GroundworkTableDeleteCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $tableName = $input->getArgument('tableName');
+        $tableName = (string) $input->getArgument('tableName');
         $em = $this->getContainer()->get('doctrine.orm.entity_manager');
         $connection = $em->getConnection();
         $connection->beginTransaction();
         try {
             $connection->query('SET FOREIGN_KEY_CHECKS=0');
-            $connection->query('DELETE FROM '.$tableName);
-            $connection->query('ALTER TABLE '.$tableName.' AUTO_INCREMENT = 1');
+
+            $connection->query('DELETE FROM :tableName');
+            $connection->query('ALTER TABLE :tableName AUTO_INCREMENT = 1');
             $connection->query('SET FOREIGN_KEY_CHECKS=1');
+            $connection->bindValue(':'.$tableName, $tableName);
         } catch (\Exception $e) {
             $connection->rollback();
         }
