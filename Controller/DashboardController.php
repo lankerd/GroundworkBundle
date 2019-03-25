db6e0b86e3c9f50eaa63e84046ecc606640f40a5
Julian Lankerd <julianlankerd@gmail.com>
2019-03-25 15:41:11 -0400

    Corrected coding standards

--- Controller/DashboardController.php
+++ Controller/DashboardController.php
@@ -10,14 +10,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
  * @package Lankerd\GroundworkBundle\Controller
  * @author  Julian Lankerd <julianlankerd@gmail.com>
  */
-
 class DashboardController extends Controller
 {
-    /**
-     * @TODO This will be removed soon!
-    */
     public function indexAction()
     {
         return $this->render('@LankerdGroundwork/MenuItems/groundwork-menu.html.twig');
     }
+
 }
