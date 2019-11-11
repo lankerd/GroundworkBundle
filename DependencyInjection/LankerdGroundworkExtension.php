c4dc40e30a041fdaed017f4ff104667b60d53d53
Julian Lankerd <julian@corephp.com>
2019-11-10 21:05:40 -0500

    Fixed all issues presented by Insights

--- DependencyInjection/LankerdGroundworkExtension.php
+++ DependencyInjection/LankerdGroundworkExtension.php
@@ -22,27 +22,6 @@ class LankerdGroundworkExtension extends Extension
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
-//
-//        $configuration = $this->getConfiguration($configs, $container);
-//        $config = $this->processConfiguration($configuration, $configs);
-//
-//        /**
-//         * lankerd_groundwork.import_services
-//         * We will set a parameter that'll hold a list
-//         * of the import service(s) the user provided
-//         */
-//        $definition = $container->getDefinition('lankerd_groundwork.import_services');
-//        $definition->setArgument(0, $config['import_services']);
-
-//        try {
-//            $container->setParameter($this->getAlias().'.import_services', $configuration['import_services']);
-//        } catch (\Exception $e) {
-//            throw $e;
-//        }
-//
-//        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-//        $loader->load('services.yml');
-
     }
 
     public function getAlias()
